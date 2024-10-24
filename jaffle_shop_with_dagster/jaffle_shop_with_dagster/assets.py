78c51e4bb2190153c565353280e187e32054f758
Gareth Brickman <50636644+garethbrickman@users.noreply.github.com>
2024-10-24 12:06:30 -0400

    add key prefix using CustomDagsterDbtTranslator

--- jaffle_shop_with_dagster/jaffle_shop_with_dagster/assets.py
+++ jaffle_shop_with_dagster/jaffle_shop_with_dagster/assets.py
@@ -1,11 +1,21 @@
-from dagster import AssetExecutionContext
-from dagster_dbt import DbtCliResource, dbt_assets
+from dagster import AssetExecutionContext, AssetKey
+from dagster_dbt import DbtCliResource, dbt_assets, DagsterDbtTranslator
+from typing import Any, Mapping
 
 from .project import jaffle_shop_project
 
 
-@dbt_assets(manifest=jaffle_shop_project.manifest_path)
+# @dbt_assets(manifest=jaffle_shop_project.manifest_path)
+# def jaffle_shop_dbt_assets(context: AssetExecutionContext, dbt: DbtCliResource):
+#     yield from dbt.cli(["build"], context=context).stream()
+
+class CustomDagsterDbtTranslator(DagsterDbtTranslator):
+    def get_asset_key(self, dbt_resource_props: Mapping[str, Any]) -> AssetKey:
+        return super().get_asset_key(dbt_resource_props).with_prefix("code_location_2")
+
+@dbt_assets(
+    manifest=jaffle_shop_project.manifest_path,
+    dagster_dbt_translator=CustomDagsterDbtTranslator(),
+)
 def jaffle_shop_dbt_assets(context: AssetExecutionContext, dbt: DbtCliResource):
     yield from dbt.cli(["build"], context=context).stream()
-    
-
