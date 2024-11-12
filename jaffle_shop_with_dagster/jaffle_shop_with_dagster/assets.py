1cb0a118ee7845b1e3460e15e9b18611657d44f2
Gareth Brickman <50636644+garethbrickman@users.noreply.github.com>
2024-11-12 11:23:40 -0500

    use --log-format json

--- jaffle_shop_with_dagster/jaffle_shop_with_dagster/assets.py
+++ jaffle_shop_with_dagster/jaffle_shop_with_dagster/assets.py
@@ -5,17 +5,10 @@ from typing import Any, Mapping
 from .project import jaffle_shop_project
 
 
-# @dbt_assets(manifest=jaffle_shop_project.manifest_path)
-# def jaffle_shop_dbt_assets(context: AssetExecutionContext, dbt: DbtCliResource):
-#     yield from dbt.cli(["build"], context=context).stream()
-
-class CustomDagsterDbtTranslator(DagsterDbtTranslator):
-    def get_asset_key(self, dbt_resource_props: Mapping[str, Any]) -> AssetKey:
-        return super().get_asset_key(dbt_resource_props).with_prefix("code_location_2")
-
-@dbt_assets(
-    manifest=jaffle_shop_project.manifest_path,
-    dagster_dbt_translator=CustomDagsterDbtTranslator(),
-)
+@dbt_assets(manifest=jaffle_shop_project.manifest_path)
 def jaffle_shop_dbt_assets(context: AssetExecutionContext, dbt: DbtCliResource):
-    yield from dbt.cli(["build"], context=context).stream()
+    dbt_command = [
+        "run",
+        "--log-format", "json"
+    ]
+    yield from dbt.cli(dbt_command, context=context).stream()
